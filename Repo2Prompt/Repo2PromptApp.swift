aae329b2097fffd0f8d2c2fe162ebbc8513a2cce
ryanlyn <mailboxryanlin@icloud.com>
2026-03-31 17:06:18 +1100

    Initial Commit

---
+++ Repo2Prompt/Repo2PromptApp.swift
@@ -0,0 +1,17 @@
+//
+//  Repo2PromptApp.swift
+//  Repo2Prompt
+//
+//  Created by Ryan on 31/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct Repo2PromptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
